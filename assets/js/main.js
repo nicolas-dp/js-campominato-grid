129bb7363da608eb5bbc538d0914e19a4ee5b0e2
Nicolas <wellcomenicolas@gmail.com>
2022-04-05 09:35:40 +0200

    Project ? -
    Manca l'associazione dei livelli con la griglia

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,9 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
-
-
-const inputElement = document.querySelector(".form-select").value
-
-inputElement.addEventListener("submit", function () {
-    selectElements(".cell", "active")
-    //Genera numeri da 1 a 100
-    generateGrid(".cells", "div", "cell", 64)
-    
-})
-
+let difficult_low = 100;
+let difficult_medium = 81;
+let difficult_high = 61;
 
 /**
  * 
@@ -24,86 +16,94 @@ inputElement.addEventListener("submit", function () {
  * @param {string} tag_name a tag name
  * @param {string} class_name a class name
  */
-function generateGrid(selector, tag_name, class_name, limit) {
+function generateGrid(number_of_cells, selector, element_name, class_name) {
     const cellsElement = document.querySelector(selector)
-
-    for (let i = 1; i <= limit; i++) {
-        const cellItem = document.createElement(tag_name)
-        cellItem.classList.add(class_name)
-        cellsElement.append(cellItem)
+    for (let i = 1; i <= number_of_cells; i++) {
+        const cell = document.createElement(element_name)
+        cell.classList.add(class_name)
+        cellsElement.append(cell)
     }
 }
 
-/**
- * 
- * @param {string} selector a css selector
- * @param {string} active_class a css class name
- */
-function selectElements(selector, active_class) {
-    const cells = document.querySelectorAll(selector);
-    const numbers = generateCellsNumber();
+function selectCells(selector) {
+    const cells = document.querySelectorAll(selector)
+    return cells
+}
 
-    for (let i = 0; i < cells.length; i++) {
-        const cell = cells[i];
-
-        //Add random number
-        const spanElement = document.createElement("span")
-        spanElement.append(numbers[i])
-        cell.append(spanElement)
-
-        cell.addEventListener("click", function () {
-            this.firstChild.style.opacity = 1;
-            if (isEven(Number(this.innerText))) {
-                this.classList.add("active_green")
-            } else {
-                this.classList.add("active_red")
-            }
+//Attivazione colore al click
+function activateCell(selector, class_name) {
+    const cells = selectCells(selector)
+    for (let index = 0; index < cells.length; index++) {
+        const cell = cells[index];
+        cell.addEventListener('click', function () {
+            cell.classList.add("selected")
         })
     }
 }
 
 
 
-/* Generare numeri casuali */
-
-/**
- * 
- * @param {number} min Minumum number to generate
- * @param {number} max Max number
- * @returns {number}
- */
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function generateCellsNumber() {
-    const randomNumbers = [];
 
-    // generare numeri casuali unici in base al valore preso dal input value
+function generateGridNumbers(min, max) {
+    const gridNumbers = []
 
-    while (randomNumbers.length !== inputElement) {
-        const randomNumber = getRandomInteger(1, inputElement)
-        if (!randomNumbers.includes(randomNumber)) {
-            randomNumbers.push(randomNumber)
+    while (gridNumbers.length !== max) {
+        const randomNumber = getRandomInteger(min, max)
+        //Esclusività di un numero nella griglia
+        if (!gridNumbers.includes(randomNumber)) {
+            gridNumbers.push(randomNumber)
         }
     }
-    return randomNumbers;
+    return gridNumbers
 }
 
 
-function isEven(number) {
-    if (number % 2 === 0) {
-        return true;
+function isEven(n) {
+    if (n % 2 === 0) {
+        return true
     } else {
-        return false;
-    }
+        return false
 
+    }
 }
 
+function fillCells(selector) {
+    const cells = selectCells(selector)
+    //console.log(cells);
+    for (let index = 0; index < cells.length; index++) {
+        const cell = cells[index];
+        //console.log(cell);
+        const gridNumbers = generateGridNumbers(1, max)
+        cell.innerHTML = `<span>${gridNumbers[index]}</span>`
+    }
+}
+
+const elementButton = document.querySelector('.btn_genera');
+
+elementButton.addEventListener("click", function () {
+    let option_value = document.getElementById("mylist").value;
+    console.log(option_value);
+
+    if (option_value == 1) {
+        generateGridNumbers(1, difficult_low)
+        generateGrid(difficult_low, '.cells', 'div', 'cell')
+        activateCell('.cell', 'selected')
+        fillCells('.cell')
+    } else if (option_value == 2) {
+        generateGridNumbers(1, difficult_medium)
+        generateGrid(difficult_medium, '.cells', 'div', 'cell')
+        activateCell('.cell', 'selected')
+        fillCells('.cell')
+    } else {
+        generateGridNumbers(1, difficult_hard)
+        generateGrid(difficult_hard, '.cells', 'div', 'cell')
+        activateCell('.cell', 'selected')
+        fillCells('.cell')
+    }
+})
 
 
-
-
-
-
-console.log(generateCellsNumber());
